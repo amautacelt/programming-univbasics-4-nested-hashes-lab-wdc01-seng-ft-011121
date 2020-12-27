2b6212ffe146addf1ca262008d1d0eecc102cd27
C. L <amautacelt@gmail.com>
2020-12-27 05:58:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -125,5 +125,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for]
+
 end
